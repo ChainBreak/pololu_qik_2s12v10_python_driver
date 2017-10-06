9288b544b1c5549f7910ed7bd0da95e9aaa60314
ChainBreak <thomas.james.rowntree@gmail.com>
2017-10-06 14:24:58 +0000

    Read motor current

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import serial
+import time
 
 ser = serial.Serial('/dev/serial0',9600,timeout=1)
 
@@ -9,17 +10,21 @@ ser = serial.Serial('/dev/serial0',9600,timeout=1)
 #print(ser.read(5))
 
 id = 10
-buf = bytearray([170,id,13,0])
+buf = bytearray([170,id,0x08,127])
 #help(ser)
 ser.write(buf)
+try:
+    while True:
+        ser.flushInput()
+        buf = bytearray([0xaa,id,0x10])
+        ser.write(buf)
+        a = bytearray(ser.read(1))
+        print("%i mA" % (int(a[0])*150))
+except KeyboardInterrupt:
+    pass
 
-##while True:
-##    ser.flushInput()
-##    ser.flushOutput()
-##    buf = bytearray([0xaa,id,0x02])
-##    for n in buf:
-##        print(int(n))
-##    ser.write(buf)
-##    a = bytearray(ser.read(1))
-##    for n in a:
-##        print(int(n))
+
+
+buf = bytearray([170,id,0x08,0])
+#help(ser)
+ser.write(buf)
